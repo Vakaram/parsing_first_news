7517fc1867888b45c597585f65d66bcf7b5cade1
Vakaram <Vakaramm@gmail.com>
2022-10-20 16:36:49 +0500

    обавил пару файлов.

--- main.py
+++ main.py
@@ -66,3 +66,5 @@ while True:
         with open('loger.txt', 'a') as f:
             f.write(logi_in)
     time.sleep(randint(1900,3500)) # поменяй на каждые 20-30 минут видать часто новости прилетают
+
+
