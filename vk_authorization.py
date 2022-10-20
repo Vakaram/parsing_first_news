6b3a7792edaebe04310e0f98f380f5a027f87095
Vakaram <Vakaramm@gmail.com>
2022-10-20 16:26:00 +0500

    потестил работает отлично, загружаю на хероку.

---
+++ vk_authorization.py
@@ -0,0 +1,36 @@
+# #будем тут авторизовываться
+# https://oauth.vk.com/blank.html
+# access_token=vk1.a.bPS-9MQErlW1FXizjfKYopPBT-onv9gqJTRq3zNzlbSmEoGiYf7BlS-r4gqtgt_oKnu-Ebq9ngc5uGn0ZrvOlNSf-4ur0lfDoBgsV6cqSKsuzBAKKTm91YAxWnFNiN5jQTRqps05S5VHHivgY2QaoLFrikrWuIHdKp1QkVxzgghIImw_1ZWb4l6MbxlMUk5c
+# #&expires_in=86400&user_id=232691134
+#
+#
+# и новый тут
+# https://oauth.vk.com/blank.html#
+# access_token=\
+#     vk1.a.NodnE5icH63dOI8dJUhrHlAidCSqOwj4GFmosEq9t6e1UOfUXrd0teOTR_-oMXCF9PEXLI4HAOf2jTPqVDLnjm4ZszW7yRIyXyZz8cK7a-NLaHGGESXyGBu8baTs2GXIILZPLNex67sREmHyX6D0M83QBPw6MG9T0VMEg7r1YGcYdmn5zeBCQ7FYN869Wxk5
+# &expires_in=0
+# &user_id=232691134
+#
+#
+#
+#
+# супер новый от 17.10.2022
+# https://oauth.vk.com/blank.html#
+# access_token=
+# vk1.a.hDlOFri7LcNndevzbi8ndcmNO_S_ClkplMv3yKnfBWBXPVpdvx5hSfR2erFVoixNf_ydUDN_QnP749i-RUsnKpRqe8-YqTOPImZ8E7_iB8zGTrOSxWsRjal-51aaIJfmonFrveoku0u9GMQQjUL7B5O2IKfOnKeor-VXBvnDJLaN74gCB55oTz0SchjVuI0z
+#                                                                                                                                 &expires_in=0&user_id=232691134
+#
+#
+# #Вот этот уже код для группы вк сначало этот запрос
+# https://oauth.vk.com/authorize?client_id=51450590&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=offline,photos,wall,groups&response_type=token&v=5.131
+# он вернул
+# https://oauth.vk.com/blank.html#access_token=
+# vk1.a.HCmEw_i8HRfBeChsKQWwmqlXjEZqawa8FVKq_I9lwXpLY7Mtp2uJYqdoZlgY7t8jJ8_vT5FTwMVTLGkyxc1qibGQoykmNXWoA6bWbEMBbouH-O7qTBmK8fLy1m2WWNkZcN_bxM8UmKlLVDF5Cg29M7gn3SzhFxSuyLeLHMcwhbSbY3WVGdDPTrvqLDDWz9pI
+# &expires_in=0&user_id=232691134
+
+
+token = "vk1.a.HCmEw_i8HRfBeChsKQWwmqlXjEZqawa8FVKq_I9lwXpLY7Mtp2uJYqdoZlgY7t8jJ8_vT5FTwMVTLGkyxc1qibGQoykmNXWoA6bWbEMBbouH-O7qTBmK8fLy1m2WWNkZcN_bxM8UmKlLVDF5Cg29M7gn3SzhFxSuyLeLHMcwhbSbY3WVGdDPTrvqLDDWz9pI"
+# token = "vk1.a.hDlOFri7LcNndevzbi8ndcmNO_S_ClkplMv3yKnfBWBXPVpdvx5hSfR2erFVoixNf_ydUDN_QnP749i-RUsnKpRqe8-YqTOPImZ8E7_iB8zGTrOSxWsRjal-51aaIJfmonFrveoku0u9GMQQjUL7B5O2IKfOnKeor-VXBvnDJLaN74gCB55oTz0SchjVuI0z"
+application_id = 51450590 #в приложение брать
+owner_id_group = -216540661 #знак минус для id группы
+
